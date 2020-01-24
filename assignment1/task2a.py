fix: epsilon in gradient approximation test should be 1e-2 not 10e-2

assignment1/task2a.py
@@ -72,7 +72,7 @@ def gradient_approximation_test(model: BinaryModel, X: np.ndarray, Y: np.ndarray
         Details about this test is given in the appendix in the assignment.
     """
     w_orig = model.w.copy()
-    epsilon = 10e-2
+    epsilon = 1e-2
     for i in range(w_orig.shape[0]):
         orig = model.w[i].copy()
         model.w[i] = orig + epsilon

assignment1/task4a.py
@@ -73,7 +73,7 @@ def gradient_approximation_test(model: SoftmaxModel, X: np.ndarray, Y: np.ndarra
         Numerical approximation for gradients. Should not be edited. 
         Details about this test is given in the appendix in the assignment.
     """
-    epsilon = 10e-2
+    epsilon = 1e-2
     for i in range(model.w.shape[0]):
         for j in range(model.w.shape[1]):    
             orig = model.w[i, j].copy()