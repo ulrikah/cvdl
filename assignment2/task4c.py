743ff401b44078d3433db08d63ffa0a0b9466a59
Haakon Hukkelaas <hakon.hukkelas@ntnu.no>
2020-01-31 13:35:53 +0100

    add: starter code for assignment 2

---
+++ assignment2/task4c.py
@@ -0,0 +1,37 @@
+import numpy as np
+import utils
+from task2a import one_hot_encode, pre_process_images, SoftmaxModel, gradient_approximation_test
+
+
+if __name__ == "__main__":
+    # Simple test on one-hot encoding
+    Y = np.zeros((1, 1), dtype=int)
+    Y[0, 0] = 3
+    Y = one_hot_encode(Y, 10)
+    assert Y[0, 3] == 1 and Y.sum() == 1, \
+        f"Expected the vector to be [0,0,0,1,0,0,0,0,0,0], but got {Y}"
+
+    X_train, Y_train, *_ = utils.load_full_mnist(0.1)
+    X_train = pre_process_images(X_train)
+    Y_train = one_hot_encode(Y_train, 10)
+    assert X_train.shape[1] == 785,\
+        f"Expected X_train to have 785 elements per image. Shape was: {X_train.shape}"
+
+    # Modify your network here
+    neurons_per_layer = [64, 64, 10]
+    use_improved_sigmoid = True
+    use_improved_weight_init = True
+    model = SoftmaxModel(
+        neurons_per_layer, use_improved_sigmoid, use_improved_weight_init)
+    logits = model.forward(X_train)
+    np.testing.assert_almost_equal(
+        logits.mean(), 1 / 10,
+        err_msg="Since the weights are all 0's, the softmax activation should be 1/10")
+
+    # Gradient approximation check for 100 images
+    X_train = X_train[:100]
+    Y_train = Y_train[:100]
+    for i in range(2):
+        if i != 0:
+            gradient_approximation_test(model, X_train, Y_train)
+        model.ws = [np.random.randn(*w.shape) for w in model.ws]
