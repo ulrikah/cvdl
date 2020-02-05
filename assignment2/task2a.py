add: weight initialization to task2a/4c to ensure that weights are not 0 - initializing weights to 0 might lead approximation test to give correct result even though gradients  are not correct

assignment2/task2a.py
@@ -150,12 +150,11 @@ if __name__ == "__main__":
     use_improved_weight_init = False
     model = SoftmaxModel(
         neurons_per_layer, use_improved_sigmoid, use_improved_weight_init)
-    logits = model.forward(X_train)
-    np.testing.assert_almost_equal(
-        logits.mean(), 1/10,
-        err_msg="Since the weights are all 0's, the softmax activation should be 1/10")
 
     # Gradient approximation check for 100 images
     X_train = X_train[:100]
     Y_train = Y_train[:100]
+    for layer_idx, w in enumerate(model.ws):
+        model.ws[layer_idx] = np.random.uniform(-1, 1, size=w.shape)
+
     gradient_approximation_test(model, X_train, Y_train)