4c0ad2a70b409f11195ce2c20c374950cafbfbaa
ulrikah <ulrikhalmoy@gmail.com>
2020-04-17 10:22:52 +0200

    remove debug logs + update server configs

--- assignment4/SSD/ssd/modeling/detector.py
+++ assignment4/SSD/ssd/modeling/detector.py
@@ -21,7 +21,6 @@ class SSDDetector(nn.Module):
             f"SSD Head number of parameters: {torch_utils.format_params(self.box_head)}")
 
     def forward(self, images, targets=None):
-        import pdb; pdb.set_trace()
         features = self.backbone(images)
         detections, detector_losses = self.box_head(features, targets)
         if self.training:
